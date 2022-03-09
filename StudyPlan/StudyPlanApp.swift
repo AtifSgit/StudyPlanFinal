3a4f0bc145c5f575d684a46361bab1b8f0a42f7d
Atif Said <atifalisaid@outlook.com>
2022-03-09 10:34:26 +0000

    Initial Commit

---
+++ StudyPlan/StudyPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyPlanApp.swift
+//  StudyPlan
+//
+//  Created by ATIF on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct StudyPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
